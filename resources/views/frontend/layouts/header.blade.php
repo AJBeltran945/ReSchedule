1b64828b86e0ebd771734c47d6500dcd831b0e29
AJBletran94 <ajbeltran945@gmail.com>
2025-05-22 05:51:01 +0200

    notification tab done

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -1,4 +1,4 @@
-<header x-data="{ open: false }" class="bg-midnight-dark border-b border-gray-700 text-white shadow">
+<header x-data="{ open: false }" class="bg-midnight-dark text-white shadow">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between items-center h-16">
 
@@ -27,7 +27,7 @@
 
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-midnight hover:text-royal focus:outline-none transition">
+                        <button class="inline-flex items-center px-3 py-2 text-sm font-medium rounded-md text-white hover:text-royal focus:outline-none transition">
                             <div>{{ Auth::user()->name }}</div>
                             <svg class="ml-2 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                 <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
